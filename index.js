a51091e539220c33ecc76016792e54c1e5c2310d
douruxin <douruxin@foxmail.com>
2024-04-25 17:09:20 +0800

    feat: 更新为 TypeScript 版本

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Greet function.
- * @param {string} name - The name to greet.
- * @returns {string} The greeting message.
- */
-export const greet = (name) => {
-  return `Hello, ${name}!`;
-};
